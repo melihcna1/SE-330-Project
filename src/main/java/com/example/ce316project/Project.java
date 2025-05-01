3d4e637ea8d41c2c89bb639ef7fae884de2e8a6a
huseyinerenceyhan <huseyinerenceyhan@gmail.com>
2025-05-01 14:49:18 +0300

    added resultDir field

--- src/main/java/com/example/ce316project/Project.java
+++ src/main/java/com/example/ce316project/Project.java
@@ -7,15 +7,17 @@ public class Project {
     private String projectName;
     private String configurationPath;
     private String submissionsDir;
+    private String resultDir;
     private TestCase testCase;  // inputFile & expectedOutputFile
     private List<StudentResult> results;
     private String schemaVersion = "1.0";
 
-    public Project(String projectName, String configurationPath, String submissionsDir, TestCase testCase) {
+    public Project(String projectName, String configurationPath, String submissionsDir, String resultDir, TestCase testCase) {
         this.projectName = projectName;
         this.configurationPath = configurationPath;
         this.submissionsDir = submissionsDir;
         this.testCase = testCase;
+        this.resultDir = resultDir;
     }
 
     public String getProjectName() {
