e8d1d0244699706ce1bfd6d775f31cbb04787dc0
huseyinerenceyhan <huseyinerenceyhan@gmail.com>
2025-05-01 14:21:18 +0300

    Controller for ProjectScreen
    may need to move into MainController

---
+++ src/main/java/com/example/ce316project/ProjectScreen.java
@@ -0,0 +1,160 @@
+package com.example.ce316project;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class ProjectScreen extends Application {
+
+    Stage thisStage;
+
+
+    @FXML
+    public TextField txtfieldProjectName, txtfieldConfiguration, txtfieldInputFile, txtfieldexpectedOutputFile, txtfieldSubmissionFolder;
+    public Button btnConfiguration, btnInputFile, btnExpectedOutputFile, btnSubmissionFolder, btnSaveProject;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProjectScreen.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            stage.setScene(scene);
+            thisStage = stage;
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    private void getConfiguration() {
+        txtfieldConfiguration.clear();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Configuration");
+        File initialDirectory = new File("src/CONFIGURATIONS");
+        fileChooser.setInitialDirectory(initialDirectory);
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON", "*.json"));
+        File file = fileChooser.showOpenDialog(thisStage);
+        if (file != null) {
+            txtfieldConfiguration.setText(file.getPath());
+        }
+
+    }
+
+    @FXML
+    private void getInputFile() {
+        txtfieldInputFile.clear();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Input File");
+
+        File file = fileChooser.showOpenDialog(thisStage);
+        if (file != null) {
+            txtfieldInputFile.setText(file.getPath());
+        }
+
+    }
+
+    @FXML
+    private void getExpectedOutputFile() {
+        txtfieldexpectedOutputFile.clear();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Output File");
+
+        File file = fileChooser.showOpenDialog(thisStage);
+        if (file != null) {
+            txtfieldexpectedOutputFile.setText(file.getPath());
+        }
+
+    }
+
+    @FXML
+    private void getSubmissionFolder() {
+        txtfieldSubmissionFolder.clear();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File folder = directoryChooser.showDialog(thisStage);
+        if (folder != null) {
+            txtfieldSubmissionFolder.setText(folder.getPath());
+        }
+
+    }
+
+    @FXML
+    private void saveProject() {
+        if (txtfieldProjectName.getText().isBlank()) {
+            showAlert("NAME IS EMPTY", "Please Enter the Project Name.");
+        } else if (txtfieldConfiguration.getText().isBlank()) {
+            showAlert("CONFIGURATION IS EMPTY", "Please Enter the Configuration.");
+        } else if (txtfieldInputFile.getText().isBlank()) {
+            showAlert("INPUT FILE IS EMPTY", "Please Enter the Input File.");
+
+        } else if (txtfieldexpectedOutputFile.getText().isBlank()) {
+            showAlert("EXPECTED OUTPUT FILE IS EMPTY", "Please Enter the Expected Output File.");
+
+        } else if (txtfieldSubmissionFolder.getText().isBlank()) {
+            showAlert("SUBMISSION FOLDER IS EMPTY", "Please Enter the Submission Folder.");
+
+        } else {
+
+            try {
+                String s = "src/PROJECTS/";
+                s += txtfieldProjectName.getText() + ".json";
+                Path path = Paths.get(s);
+                File projectFile = Files.createFile(path).toFile();
+                Project.TestCase testCase = new Project.TestCase(txtfieldInputFile.getText(), txtfieldexpectedOutputFile.getText());
+                Project project = new Project(txtfieldProjectName.getText(), txtfieldConfiguration.getText(), txtfieldSubmissionFolder.getText(), testCase);
+                ProjectIO.save(project, path);
+            } catch (FileAlreadyExistsException e) {
+                showAlert("FILE ALREADY EXISTS", "Please Enter a different name or delete the other file.");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+
+
+
+
+
+
