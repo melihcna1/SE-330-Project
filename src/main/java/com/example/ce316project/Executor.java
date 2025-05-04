e60c06c9ba28b173df85b82cfc489ca02c078f28
davsan18 <120686590+davsan18@users.noreply.github.com>
2025-05-04 13:55:33 +0300

    executor update

--- src/main/java/com/example/ce316project/Executor.java
+++ src/main/java/com/example/ce316project/Executor.java
@@ -1,51 +1,68 @@
 package com.example.ce316project;
 
-import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Executor {
     Configuration c;
     File[] files;
+    Process[] p;
     public Executor(Configuration c,File[] files) {
         this.c = c;
         this.files = files;
+        p = new Process[files.length];
     }
-    private String[] execute(String cmd) {     //runs the operation at addresses with the cmd in configuration and returns all the outputs array. Don't have the time to test it
-        String[] output = new String[files.length];
-        String s;
+    private void execute(String cmd,String log) {     //runs the operation at addresses with the cmd in configuration and returns all the outputs array. Don't have the time to test it
         ProcessBuilder pb = new ProcessBuilder(cmd);
-        int count=0;
-        for (File address:files) {
-            output[count]="";
-            pb.directory(address);
-            pb.redirectErrorStream(true);
+        for (int i=0;i<files.length;i++) {
+            pb.directory(files[i]);
+            pb.redirectOutput(new File(files[i],log+"Log.txt"));
+            pb.redirectError(new File(files[i],log+"Err.txt"));
             try {
-                Process p = pb.start();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                while ((s=reader.readLine())!=null) {
-                    output[count] += s;
-                }
-            } catch (IOException ex) {
-                output[count] = ex.getMessage();
-            }
-            count++;
+                p[i] = pb.start();
+            } catch (IOException ex) {}
         }
-        return output;
     }
     
-    public void compile() {
-            String[] result = execute(c.getCompileCmd());
-    }
+    public StudentResult[] run(String solution) throws FileNotFoundException, InterruptedException {
+        StudentResult[] results = new StudentResult[files.length];
+        if (c.getCompileCmd()!=null) {
+            execute(c.getCompileCmd(),"compoile");
+        }
+        for (int i=0;i<p.length;i++) {
+            p.wait();
+            if (p[i].exitValue()!=0) {
 
-    public void run() {
-        String[] result = execute(c.getRunCmd());
-        for (String s:result) {
-            s.compareTo(s);
+            }
         }
+        execute(c.getRunCmd(),"run");
+        for (int i=0;i<results.length;i++) {
+            String err = "";
+            String output = "";
+            try {
+                p[i].wait();
+            } catch (InterruptedException ex) {
+            }
+            Scanner logScanner = new Scanner(new File(files[i],"runLog.txt"));
+            while (logScanner.hasNext()) {
+                output += logScanner.nextLine();
+            }
+            Scanner errScanner = new Scanner(new File(files[i],"runErr.txt"));
+            while (errScanner.hasNext()) {
+                err += errScanner.nextLine();
+            }
+            results[i] = new StudentResult(files[i].getName(),"",err , output, "", 10);
+            if (solution.isBlank()) {
+                solution = output;
+            }
+            if (output==solution) {
+                results[i].setStatus("passed");
+            }
+            else results[i].setStatus("failed");
+        }
+        return results;
     }
-    
-    //------------------------------------------------------- WARNING: Code not done yet, still in progress. -------------------------------------------------------
 
 }
