617a95641d8fa8ac5c701d2de349a1a4ae459c68
davsan18 <120686590+davsan18@users.noreply.github.com>
2025-05-04 22:55:54 +0300

    Executor prototype done

--- src/main/java/com/example/ce316project/Executor.java
+++ src/main/java/com/example/ce316project/Executor.java
@@ -4,21 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Executor {
     Configuration c;
     File[] files;
     Process[] p;
+    boolean[] notCompiled;
     public Executor(Configuration c,File[] files) {
         this.c = c;
         this.files = files;
         p = new Process[files.length];
+        notCompiled = new boolean[files.length];
     }
-    private void execute(String cmd,String log) {     //runs the operation at addresses with the cmd in configuration and returns all the outputs array. Don't have the time to test it
-        ProcessBuilder pb = new ProcessBuilder(cmd);
+    private void execute(List<String> cmd,String log) {    //runs the operation at addresses with the cmd in configuration and returns all the outputs array. Don't have the time to test it
         for (int i=0;i<files.length;i++) {
-            if (files[i]!=null) {
+            if (!notCompiled[i]) {
+                ProcessBuilder pb = new ProcessBuilder(cmd);
                 pb.directory(files[i]);
                 pb.redirectOutput(new File(files[i],log+"Log.txt"));
                 pb.redirectError(new File(files[i],log+"Err.txt"));
@@ -29,40 +32,67 @@ public class Executor {
         }
     }
     
-    public StudentResult[] run(String solution) throws InterruptedException, IOException {
+    public StudentResult[] run(String solution,List<String> args) throws InterruptedException, IOException {
         StudentResult[] results = new StudentResult[files.length];
-        if (c.getCompileCmd()!=null) {
-            execute(c.getCompileCmd(),"compile");
+        ArrayList<String> argst = new ArrayList<String>();
+        if (!c.getCompileCmd().isEmpty()) {
+            argst.add(c.getCompileCmd());
+            execute(argst,"compile");
         }
         for (int i=0;i<p.length;i++) {
-            p.wait();
+            p[i].waitFor();
             if (p[i].exitValue()!=0) {
-                files[i]=null;
+                notCompiled[i]=true;
             }
         }
-        execute(c.getRunCmd(),"run");
+        argst.clear();
+        argst.add(c.getRunCmd());
+        argst.addAll(args);
+        execute(argst,"run");
         for (int i=0;i<results.length;i++) {
             String err = "";
             String output = "";
-            try {
-                p[i].wait();
-            } catch (InterruptedException ex) {
+            String comErr = "";
+            String comOut = "";
+            String l;
+            BufferedReader logBufferedReader = new BufferedReader(new FileReader(files[i].getAbsolutePath()+"/compileLog.txt"));
+            while ((l = logBufferedReader.readLine())!=null) {
+                comOut += l;
             }
-            if (files[i]!=null) {
-                BufferedReader logBufferedReader = new BufferedReader(new FileReader(files[i].getAbsolutePath()+"/runLog.txt"));
-                while (logBufferedReader.ready()) {
-                    output += logBufferedReader.read();
+            BufferedReader errBufferedReader = new BufferedReader(new FileReader(files[i].getAbsolutePath()+"/compileErr.txt"));
+            while ((l = errBufferedReader.readLine())!=null) {
+                comErr += l;
+            }
+            if (!notCompiled[i]) {
+                try {
+                    p[i].waitFor();
+                } 
+                catch (InterruptedException ex) {
                 }
-                BufferedReader errBufferedReader = new BufferedReader(new FileReader(files[i].getAbsolutePath()+"/runErr.txt"));
-                while (errBufferedReader.ready()) {
-                    err += errBufferedReader.read();
+                
+                logBufferedReader = new BufferedReader(new FileReader(files[i].getAbsolutePath()+"/runLog.txt"));
+                while ((l = logBufferedReader.readLine())!=null) {
+                    output += l;
+                }
+                errBufferedReader = new BufferedReader(new FileReader(files[i].getAbsolutePath()+"/runErr.txt"));
+                while ((l = errBufferedReader.readLine())!=null) {
+                    err += l;
                 }
             }
-            results[i] = new StudentResult(files[i].getName(),"",err , output, "", 10);
-            if (output==solution) {
+            results[i] = new StudentResult(files[i].getName(),"",comErr , comOut, "", 0);
+            if (notCompiled[i]) {
+                results[i].setStatus("Compile fail");
+            }
+            else {
+                results[i].setErrors(err);
+                results[i].setLog(output);
+                if (output.equals(solution)) {
                 results[i].setStatus("passed");
+                }
+                else results[i].setStatus("failed");
             }
-            else results[i].setStatus("failed");
+            errBufferedReader.close();
+            logBufferedReader.close();
         }
         return results;
 
